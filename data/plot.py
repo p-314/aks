2b0e2699cd1ac9a5ac0145d5df9371e0256b5b0b
p-314 <pascalmschulze@gmail.com>
2023-06-29 14:18:25 +0200

    python plotting

--- data/plot.py
+++ data/plot.py
@@ -1,12 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-f_num = open('big.txt', 'r')
-f_t = open('times.txt', 'r')
+f_num = open('data/test.txt', 'r')
+f_t = open('data/times_small.txt', 'r')
 
-x = [int(l) for l in f_num.read().splitlines()]
-y = [float(l) for l in f_t.read().splitlines()]
+x = np.array([int(l) for l in f_num.read().splitlines()])
+y = np.array([float(l) for l in f_t.read().splitlines()])
 
-plt.plot(x, y, '.')
-plt.xscale('log')
+A = np.vstack([np.log2(np.log2(x))[100:], np.ones(len(x[100:]))]).T
+m, c = np.linalg.lstsq(A, np.log2(y)[100:], rcond=None)[0]
+
+print(m, c)
+
+plt.loglog(np.log2(x), 2**(np.log2(np.log2(x))*m+c))
+plt.loglog(np.log2(x), y, 'k.', markersize=3)
+#plt.yscale('log')
+plt.show()
+
+plt.plot(np.log2(x), y, 'k.', markersize=3)
+plt.plot(np.log2(x), np.log2(x)**m*2**c)
 plt.show()
